4567f22a54e7b093e8ff1e3623b34a83c6d0bc53
ajfkdk <56441998+ajfkdk@users.noreply.github.com>
2024-02-22 23:01:33 +0800

    失败1，没有模拟出HID设备

--- app/src/main/java/com/tencent/blue/MainActivity.java
+++ app/src/main/java/com/tencent/blue/MainActivity.java
@@ -2,55 +2,76 @@ package com.tencent.blue;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.bluetooth.BluetoothAdapter;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.Toast;
 
-import com.tencent.blue.blueTooth.AcceptThread;
-import com.tencent.blue.blueTooth.BluetoothService;
-import com.tencent.blue.blueTooth.HidDeviceDemo;
+import com.tencent.blue.blueModule.Simulator.IKeyboardSimulator;
+import com.tencent.blue.blueModule.Simulator.impl.KeyboardSimulator;
+import com.tencent.blue.blueModule.manager.IBluetoothConnectionManager;
+import com.tencent.blue.blueModule.manager.impl.BluetoothConnectionManager;
+import com.tencent.blue.blueModule.utils.KeyCode;
 
 public class MainActivity extends AppCompatActivity {
-    HidDeviceDemo hid;
+    // 创建蓝牙连接管理器
+    IBluetoothConnectionManager connectionManager;
+
+    // 创建键盘模拟器，注入连接管理器
+    IKeyboardSimulator keyboard;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // 初始化蓝牙连接管理器
+        connectionManager = new BluetoothConnectionManager(this);
+        // 初始化键盘模拟器
+        keyboard = new KeyboardSimulator(connectionManager);
 
+        // 连接设备
+        connectionManager.waitToConnect();
+        Button sendSingle = findViewById(R.id.sendSingle);
+        sendSingle.setOnClickListener(v -> {
 
-        BluetoothService bs = new BluetoothService(this);
-        //获取textview
-        TextView info = findViewById(R.id.info);
+            // 如果设备已连接，发送按键
+            if (connectionManager.isConnected()) {
+                keyboard.pressKey(KeyCode.KEY_A); // 按下A键
+                keyboard.releaseKey(); // 释放
+            }else{
+                Toast.makeText(this, "设备未连接", Toast.LENGTH_SHORT).show();
+            }
 
-        //获取debug按钮
-        Button debug = findViewById(R.id.debug);
-
-        hid = new HidDeviceDemo();
-        hid.init(bs.getAdapter(), this);
-        //设置按钮点击事件
-        debug.setOnClickListener(v -> {
-            Log.d("main", "onCreate: chenjunbin");
-            hid.sendKeys("chenjunbin");
         });
 
-        Button Server = findViewById(R.id.server);
-        AcceptThread acceptThread = new AcceptThread(bs.getAdapter(), this);
-        Server.setOnClickListener(v -> {
-            hid.test();
+        Button sendCombination = findViewById(R.id.sendCombination);
+        sendCombination.setOnClickListener(v -> {
+            if (connectionManager.isConnected()) {
+                int[] keyCodes = new int[]{KeyCode.MODIFIER_LEFT_CTRL, KeyCode.KEY_A};
+                keyboard.sendCombination(keyCodes);
+                keyboard.releaseKey();
+            }else{
+                Toast.makeText(this, "设备未连接", Toast.LENGTH_SHORT).show();
+            }
         });
 
-
-
+        Button sendText = findViewById(R.id.sendString);
+        sendText.setOnClickListener(v -> {
+            if (connectionManager.isConnected()) {
+                keyboard.sendText("with his song!");
+            }else{
+                Toast.makeText(this, "设备未连接", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        hid.close();
+        Toast.makeText(this, "断开连接", Toast.LENGTH_SHORT).show();
+        connectionManager.disconnect();
+
     }
 
 
